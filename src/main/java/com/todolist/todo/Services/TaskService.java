d9cb7d80bfd13b3098bee0c7f088acb78ccba8d6
Denkaan <dennis.petreski@hotmail.com>
2022-03-06 23:11:40 +0100

    commit

--- src/main/java/com/todolist/todo/Services/TaskService.java
+++ src/main/java/com/todolist/todo/Services/TaskService.java
@@ -1,10 +1,58 @@
 package com.todolist.todo.Services;
 
+
 import com.todolist.todo.Models.Task;
+import com.todolist.todo.Repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface TaskService {
-    public Task saveTask(Task task);
-    public List<Task> getAllTasks();
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    //POST
+    public Task saveTask(Task task) {
+        System.out.println(task.toString());
+        return taskRepository.save(task);
+    }
+
+    //GET
+    public List<Task> getTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public List<Task> getTaskByTitle(String title) {
+        return taskRepository.findByTitle(title);
+    }
+
+    /*
+    public List<Task> getTasksForUser(String email) {
+        return taskRepository.findAllByEmail(email);
+    }
+
+     */
+
+    //PUT
+    public Task updateTask(Task task) {
+        System.out.println("updates");
+        Task existingTask = taskRepository.findById(task.getId()).orElse(null);
+        existingTask.setTitle(task.getTitle());
+        existingTask.setDescription(task.getDescription());
+        existingTask.setDate(task.getDate());
+        existingTask.setTime(task.getTime());
+        return taskRepository.save(existingTask);
+    }
+
+    //DELETE
+    public String deleteTask(long id) {
+        taskRepository.deleteById(id);
+        return id + " id -> task removed/completed";
+    }
 }
--- src/main/java/com/todolist/todo/Services/UserService.java
+++ src/main/java/com/todolist/todo/Services/UserService.java
@@ -1,7 +1,47 @@
 package com.todolist.todo.Services;
 
 import com.todolist.todo.Models.User;
+import com.todolist.todo.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    public User saveUser(User user);
+import javax.persistence.NonUniqueResultException;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUser(User user) {
+        System.out.println("Service GET *****");
+        return userRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+    }
+    public boolean getUserByEmail(String email, String password) {
+        boolean email_present;
+        boolean password_present;
+        try {
+            email_present = userRepository.findTopByEmail(email) != null ? true : false;
+            System.out.println("Email present: " + email_present);
+            password_present = userRepository.findTopByPassword(password) != null ? true : false;
+            System.out.println("Password present: " + password_present);
+        } catch(NonUniqueResultException nre) {
+            return true;
+        }
+        return email_present && password_present;
+    }
+
+    public boolean findUserByEmail(String email) {
+        boolean email_present;
+        try {
+            email_present = userRepository.findTopByEmail(email) != null ? true : false;
+            System.out.println("Username present (U): " + email_present);
+        } catch(NonUniqueResultException nre) {
+            return true;
+        }
+        return email_present;
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
 }
